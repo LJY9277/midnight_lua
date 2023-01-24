2119a1631f6204bbacb124d273c52bff1ea8bc0b
LJY9277 <100998575+LJY9277@users.noreply.github.com>
2023-01-24 11:19:52 +0800

    Update and rename gta/history.lua to 多伦多/history.lua

---
+++ 多伦多/history.lua
@@ -0,0 +1,189 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Welcome to Ubuntu 22.04.1 LTS (GNU/Linux 5.15.0-39-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of Tue Jan 24 02:54:17 UTC 2023
+
+  System load:              0.28759765625
+  Usage of /:               12.6% of 48.29GB
+  Memory usage:             47%
+  Swap usage:               0%
+  Processes:                122
+  Users logged in:          0
+  IPv4 address for docker0: 172.17.0.1
+  IPv4 address for eth0:    2.58.64.115
+  IPv6 address for eth0:    2405:f3c0:1:9684::1
+
+ * Strictly confined Kubernetes makes edge and IoT secure. Learn how MicroK8s
+   just raised the bar for easy, resilient and secure K8s cluster deployment.
+
+   https://ubuntu.com/engage/secure-kubernetes-at-the-edge
+
+2 updates can be applied immediately.
+2 of these updates are standard security updates.
+To see these additional updates run: apt list --upgradable
+
+
+*** System restart required ***
+Last login: Tue Jan 24 02:34:40 2023 from 116.9.23.111
+root@WIKIHOST-23012219U5FP:~# apt list --upgradable
+Listing... Done
+libruby3.0/jammy-updates,jammy-security 3.0.2-7ubuntu2.3 amd64 [upgradable from: 3.0.2-7ubuntu2.2]
+ruby3.0/jammy-updates,jammy-security 3.0.2-7ubuntu2.3 amd64 [upgradable from: 3.0.2-7ubuntu2.2]
+update-notifier-common/jammy-updates 3.192.54.3 all [upgradable from: 3.192.54]
+root@WIKIHOST-23012219U5FP:~# 
+
+
+
+
+Listing... Done
+update-notifier-common/jammy-updates 3.192.54.3 all [upgradable from: 3.192.54]
+N: There is 1 additional version. Please use the '-a' switch to see it
+
+                                                                                                          │    
+-----------------------------------------------
+
+
+
+
+
+
+
+
+
+Package configuration                                                                                               
+                                                                                                                    
+                                                                                                                    
+                                                                                                                    
+    ┌────────────────────────────────────────┤ Pending kernel upgrade ├────────────────────────────────────────┐    
+    │                                                                                                          │    
+    │ Newer kernel available                                                                                   │    
+    │                                                                                                          │    
+    │ The currently running kernel version is 5.15.0-39-generic which is not the expected kernel version       │    
+    │ 5.15.0-58-generic.                                                                                       │    
+    │                                                                                                          │    
+    │ Restarting the system to load the new kernel will not be handled automatically, so you should consider   │    
+    │ rebooting.                                                                                               │    
+    │                                                                                                          │    
+    │                                                  <Ok>                                                    │    
+    │                                                             
